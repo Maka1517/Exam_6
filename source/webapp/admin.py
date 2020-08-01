118a33468dd8748d101c548a6d237e01417c1dbb
Kurmanbek <kkmambetov@gmail.com>
2020-08-01 14:17:53 +0600

    Добавлена модель гостей, создана миграция, и модель зарегистрирована в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Guest
 
-# Register your models here.
+class GuestAdmin(admin.ModelAdmin):
+    list_filter = ('status',)
+    list_display = ('pk','name','email','updated_at','status')
+    list_display_links = ('pk', 'name')
+    search_fields = ('name',)
+
+admin.site.register(Guest,GuestAdmin)
+
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+DEFAULT_CATEGORY = ('active')
+CATEGORY_CHOICES = (
+    (DEFAULT_CATEGORY,'Активно'),
+    ('blocked','Заблокировано')
+)
+
+class Guest(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    email = models.EmailField(max_length=200, verbose_name='Почта' )
+    text = models.TextField(max_length=1000, verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    status = models.CharField(max_length=20, verbose_name='Статус', choices=CATEGORY_CHOICES, default=DEFAULT_CATEGORY)
+
+    def __str__(self):
+        return f'{self.name} - {self.email}'
