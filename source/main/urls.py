4c1c3079b7f4245e8a4dd55a0f9b2d229e654954
Kurmanbek <kkmambetov@gmail.com>
2020-08-01 18:10:09 +0600

    Добывлен update гостей

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import index_view, guest_create_view
+from webapp.views import index_view, guest_create_view, guest_update_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view, name='index'),
-    path('add', guest_create_view, name='guest_add')
+    path('add', guest_create_view, name='guest_add'),
+    path('<int:pk>/update', guest_update_view, name='guest_update')
+
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from webapp.models import Guest
 from webapp.forms import GuestForm, BROWSER_DATETIME_FORMAT
+from django.utils.timezone import make_naive
 from django.http import HttpResponseNotAllowed
 
 def index_view(request):
@@ -33,5 +34,56 @@ def guest_create_view(request):
             return render(request, 'guest_create.html', context={
                 'form': form
             })
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
+
+
+def guest_update_view(request, pk):
+    guest = get_object_or_404(Guest, pk=pk)
+    if request.method == "GET":
+        form = GuestForm(initial={
+            'name': guest.name,
+            'email': guest.email,
+            'text': guest.text,
+            'status': guest.status,
+            # форматирование перед выводом для DateTime.
+            'created_at': make_naive(guest.created_at) \
+                .strftime(BROWSER_DATETIME_FORMAT)
+            # для дат выглядит просто как:
+            # 'publish_at': article.publish_at
+        })
+        return render(request, 'guest_update.html', context={
+            'form': form,
+            'guest': guest
+        })
+    elif request.method == 'POST':
+        form = GuestForm(data=request.POST)
+        if form.is_valid():
+            # Article.objects.filter(pk=pk).update(**form.cleaned_data)
+            guest.name = form.cleaned_data['name']
+            guest.email = form.cleaned_data['email']
+            guest.text = form.cleaned_data['text']
+            guest.status = form.cleaned_data['status']
+            guest.publish_at = form.cleaned_data['created_at']
+            guest.save()
+            return redirect('index')
+        else:
+            return render(request, 'guest_update.html', context={
+                'guest': guest,
+                'form': form
+            })
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
+
+
+
+
+def guest_delete_view(request, pk):
+    guest = get_object_or_404(Guest, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'guest_delete.html', context={'guest': guest})
+    elif request.method == 'POST':
+        guest.delete()
+        return redirect('index')
     else:
         return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
